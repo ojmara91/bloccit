37369b349816347c4ed85a392cc0213419fc9a2a
Omeed Marashi <omeed32_marashi@yahoo.com>
2015-06-23 16:53:43 -0700

    topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  get 'topics/index'
+
+  get 'topics/new'
+
+  get 'topics/show'
+
+  get 'topics/edit'
+
   devise_for :users
-  resources :posts
   resources :questions
+
+  resources :topics do
+     resources :posts, except: [:index]
+   end
 
   get 'about' => 'welcome#about'
 
